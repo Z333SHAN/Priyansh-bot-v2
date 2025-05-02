ec58ec1cba2d65ef2ae5cf610854fa17bfcb2762
Z33SHAN <zeeshhan940@gmail.com>
2025-05-02 21:24:45 +0300

    Update 0_ARIF-DPZ-3.js

--- Priyansh/commands/0_ARIF-DPZ-3.js
+++ Priyansh/commands/0_ARIF-DPZ-3.js
@@ -60,7 +60,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   var tile = djtme[Math.floor(Math.random() * djtme.length)];
 
   var background = [
-  "https://i.imgur.com/nus3UBs.jpg",
+  "https://i.postimg.cc/1zS5gWg7/nus3UBs.jpg",
   ];
   var rd = background[Math.floor(Math.random() * background.length)];
 
