d27f487720132c3f33da3e61c1fe5cfdaff63cce
Z33SHAN <zeeshhan940@gmail.com>
2025-05-02 20:45:08 +0300

    Update 0_ARYAN-BRYANI.js

--- Priyansh/commands/0_ARYAN-BRYANI.js
+++ Priyansh/commands/0_ARYAN-BRYANI.js
@@ -13,15 +13,15 @@ module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils
   var name = await Users.getNameUser(event.senderID);
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
-  if(react.includes("Bryani") ||
-     react.includes("biryani") || react.includes("bryani") || react.includes("BRYANI") ||
+  if(react.includes("Biryani") ||
+     react.includes("biryani") || react.includes("bryani") || react.includes("BIRYANI") ||
 react.includes("Bryani") ||
 react.includes("BRYANI") ||     
 react.includes("bryani")) {
     var msg = {
         body: `${name} 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐀𝐏𝐊𝐈 𝐁𝐑𝐘𝐀𝐍𝐈 💐✿
-        •⊰ ☞𓅂 𝐌𝐀𝐃𝐄 ♡ 𝐁𝐘 ♡ 𝐀𝐑𝐘𝐀𝐍𓅂☜ ⊱•`,
-attachment: fs.createReadStream(__dirname + `/noprefix/Bryani.jpeg`)
+        •⊰ ☞𓅂 𝐌𝐀𝐃𝐄 ♡ 𝐁𝐘 ♡ 𝐒𝐀𝐌𝐄𝐄𝐑𓅂☜ ⊱•`,
+attachment: fs.createReadStream(__dirname + `/noprefix/0f0424c034c8912742204bb3bf13c90c.jpg`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🍗", event.messageID, (err) => {}, true)
