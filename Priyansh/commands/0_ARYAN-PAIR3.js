bc1fff93de539fbd5be7781b1c2dd7a0e58dfa37
Z33SHAN <zeeshhan940@gmail.com>
2025-05-02 21:06:58 +0300

    Update 0_ARYAN-PAIR3.js

--- Priyansh/commands/0_ARYAN-PAIR3.js
+++ Priyansh/commands/0_ARYAN-PAIR3.js
@@ -18,7 +18,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'pairing.png2');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/gk4UsTF.jpeg", path);
+    if (!existsSync(path)) await downloadFile("https://i.postimg.cc/13RjfsWg/Messenger-creation-1009607031127292.png", path);
 }
 
 async function makeImage({ one, two }) {
